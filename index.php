58c8dcd6a03fa6d7a9480f80f4291ef2aa2acec2
Ivan Dario Posada <ivandarioposada@yahoo.com>
2023-10-11 13:56:45 -0500

    Finalizacion de maqueta basica

--- index.php
+++ index.php
@@ -90,10 +90,6 @@
 
         </div>
 
-
-
-
-
     </div>
 
   </section>
@@ -104,14 +100,33 @@
       </div>
 
     <div class="row">
-      <div class="col m6 s12">
+      <div class="col m12 s12">
+            <div class="contact-form">
+              <form class="" action="index.html" method="post">
 
+
+              <div class="input-field col m12">
+                <input id="last_name" type="text" class="validate">
+                <label for="last_name">Your Name</label>
+              </div>
+
+              <div class="input-field col m12">
+                <input id="email" type="text" class="validate">
+                <label for="email">Your Email</label>
+              </div>
+
+              <div class="input-field col m12">
+                <textarea id="textarea1" class="materialize-textarea"></textarea>
+                <label for="textarea1">Your Message</label>
+              </div>
+
+              <div class="center enviar">
+       <input type="submit" name="iniciar" value="Send Message" class="waves-effect waves-light btn btn-enviar center">
+         </div>
+
+              </form>
+            </div>
       </div>
-
-      <div class="col m6 s12 img-about">
-            <img src="img/img-perfil1png.png" alt="">
-      </div>
-
     </div>
   </section>
 </main>
