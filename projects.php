f9ab38ed493f44ecd35b36ed5ea1ee36fde0fa55
Ivan Dario Posada <ivandarioposada@yahoo.com>
2023-10-13 11:28:36 -0500

    Inicio maqueta projects

---
+++ projects.php
@@ -0,0 +1,37 @@
+<?php include('header.php'); ?>
+<main>
+  <section class="intro">
+    <div class="row animate__animated animate__slideInDown" data-aos="fade-up">
+      <div class="col m12 s12 titulo-home">
+        PROJECTS
+      </div>
+    </div>
+
+    <div class="text-about animate__animated animate__slideInDown">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices iaculis sem nec sodales. In nec
+    </div>
+  </section>
+
+
+  <section class="project-list">
+    <div class="row">
+
+      <div class="col l6 m6 s12">
+
+        <div class="card card-project-int">
+          <div class="pj-project-name">
+            DelaEspriella Style Ecommerce
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col l6 m6 s12">
+
+      </div>
+
+    </div>
+  </section>
+
+</main>
+<?php include('footer.php'); ?>
