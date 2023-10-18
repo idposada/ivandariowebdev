8e3447fe6ca0ea7c2a93c2930d39595ff7650840
Ivan Dario Posada <ivandarioposada@yahoo.com>
2023-10-18 18:25:19 -0500

    Finalizacion de maqueta de interna de proyecto

---
+++ project.php
@@ -0,0 +1,67 @@
+<?php include('header.php'); ?><main>
+  <section class="intro">
+    <div class="row animate__animated animate__slideInDown" data-aos="fade-up">
+      <div class="col m12 s12 titulo-project">
+    <h1>DELAESPRIELLA STYLE ECOMMERCE</h1>
+      </div>
+    </div>
+  </section>
+
+  <section class="project-details">
+    <div class="row">
+      <div class="col m6 s12">
+        <div class="project-text">
+          With great enthusiasm, i proudly introduce 'O Plano de Metas': a landing page developed in collaboration with Supernova for the renowned @nocidanielle, who boasts an impressive community of over 888k followers.
+        </div>
+      </div>
+
+      <div class="col m6 s12">
+        <div class="project-tech">
+          Wordpress Custom template
+
+          emplate over HTML, Css3, Javascript
+
+          Front-end & back-End
+
+          Hosting and Domain COnfiguration
+        </div>
+
+      </div>
+
+    </div>
+
+  </section>
+
+  <hr>
+
+  <section class="project-imgs">
+    <div class="row">
+      <div class="project-imgs-principal">
+        <img src="img/project-img.png" alt="">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col m6 s12">
+        <div class="project-imgs-secundaria">
+          <img src="img/img-project1.png" alt="">
+        </div>
+      </div>
+
+      <div class="col m6 s12">
+        <div class="project-imgs-secundaria">
+        <img src="img/img-project1.png" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="row center">
+      <div class="project-links-int">
+        <a class="waves-effect waves-light btn btn-project">VISIT SITE</a>
+      </div>
+    </div>
+  </section>
+
+</main>
+<?php include('footer.php'); ?>
