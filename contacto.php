3695857efb07a82f7281a7cb70676cfd824ee726
Ivan Dario Posada <ivandarioposada@yahoo.com>
2023-10-26 16:01:34 -0500

    Avance seccion contacto

---
+++ contacto.php
@@ -0,0 +1,61 @@
+<?php include('header.php'); ?>
+<main>
+  <section class="intro">
+    <div class="row animate__animated animate__slideInDown" data-aos="fade-up">
+      <div class="col m12 s12 titulo-home">
+    <h1>CONTACT ME</h1>
+      </div>
+    </div>
+
+    <div class="text-about animate__animated animate__slideInDown">
+      Make thinks happen, Together!
+    </div>
+  </section>
+
+  <section class="sec-contact">
+    <div class="row">
+      <div class="col m6 s12">
+        <div class="contact-form ">
+
+
+              <form class="" action="index.html" method="post">
+
+
+              <div class="input-field ">
+                <input id="last_name" type="text" class="validate">
+                <label for="last_name">Your Name</label>
+              </div>
+
+              <div class="input-field ">
+                <input id="email" type="text" class="validate">
+                <label for="email">Your Email</label>
+              </div>
+
+              <div class="input-field ">
+                <textarea id="textarea1" class="materialize-textarea"></textarea>
+                <label for="textarea1">Your Message</label>
+              </div>
+
+              <div class="center enviar ">
+                <input type="submit" name="iniciar" value="Send Message" class="waves-effect waves-light btn btn-enviar center">
+              </div>
+
+              </form>
+                    </div>
+      </div>
+
+      <div class="col m6 s12">
+        <div class="redes">
+          <div class="">
+
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+
+</main>
+
+<?php include('footer.php'); ?>
