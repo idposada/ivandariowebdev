c4505381726e6eed70c69789bcafd3ee86f3ef3c
Ivan Dario Posada <ivandarioposada@yahoo.com>
2025-01-22 18:43:14 -0500

    Ajuste de footer y textos de menu responisve

--- footer.php
+++ footer.php
@@ -1,15 +1,13 @@
 <?php wp_footer();?>
 <footer>
-<div class="footer-line">
-  <hr class="green-lines">
-</div>
 
-<div class="footer-text">
+<div class="my-footer row" >
+<div class="footer-text col m6 s12">
   Designed and Bult By Ivan Dario Posada
   <br>
   All Right Reserved - <?php echo date("Y"); ?>
 </div>
-<div class="social-buttons center">
+<div class="social-buttons center col m6 s12">
 
   <a href="https://www.linkedin.com/in/ivan-dario-posada-suarez-9629a743/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BHT6n%2BS8RQq%2BqzcayqKrmBA%3D%3D" target="_blank" class="waves-effect waves-light btn btn-redes"><iconify-icon icon="mdi:linkedin"></iconify-icon> <span class="link.title"> </a>
 
@@ -19,7 +17,7 @@
   <a href="https://twitter.com/idposada" target="_blank" class="waves-effect waves-light btn btn-redes"><iconify-icon icon="devicon:twitter"></iconify-icon><span class="link.title"> </a>
     <a href="https://telegram/idposada" target="_blank" class="waves-effect waves-light btn btn-redes"><iconify-icon icon="cib:telegram"></iconify-icon> <span class="link.title"> </a>
 </div>
-
+</div>
 
   <!--JavaScript at end of body for optimized loading-->
   <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
